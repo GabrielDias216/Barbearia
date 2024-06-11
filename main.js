2213cf22e98e2f42de60aaf23b857ae450afb2e0
GabrielDias216 <bebel54321@icloud.com>
2024-06-11 02:42:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navbar = document.getElementById("navbar");
+
+  menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("show");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselInner = document.querySelector('.carousel-inner');
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  let currentIndex = 0;
+  const intervalTime = 3000;
+
+  function showNextSlide() {
+    currentIndex++;
+    if (currentIndex === carouselItems.length) {
+      currentIndex = 0;
+      carouselInner.style.transition = 'none';
+      carouselInner.style.transform = 'translateX(0)';
+      setTimeout(() => {
+        carouselInner.style.transition = 'transform 0.5s ease-in-out';
+      }, 50); // Adicione um pequeno atraso para garantir que a transição seja reiniciada
+    } else {
+      carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+  }
+
+  setInterval(showNextSlide, intervalTime); // Muda a foto a cada 3 segundos
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const textElement = document.querySelector('.textocarousel h1');
+  const text = " De um simples traço à FROMAS INOVADORAS!";
+  let charIndex = 0;
+  const typingSpeed = 100; // Velocidade de digitação em milissegundos
+
+  function type() {
+    if (charIndex < text.length) {
+      textElement.textContent += text.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingSpeed);
+    }
+  }
+
+  // Iniciar o efeito de digitação
+  type();
+});
